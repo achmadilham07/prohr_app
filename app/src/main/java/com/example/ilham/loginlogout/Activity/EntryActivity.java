67bb34367fcdba3ffc3181266e0b3263753a8a97
alfinnur <36445393+alfinnur@users.noreply.github.com>
2018-02-15 22:54:19 +0700

    Floating Action Button
    +Perbaiki fitur floating button.
    +backbutton close konten
    -konten masih kosong
    -tampilan masih biasa
    -belum bisa close apabila ditekan diluar konten

--- app/src/main/java/com/example/ilham/loginlogout/Activity/EntryActivity.java
+++ app/src/main/java/com/example/ilham/loginlogout/Activity/EntryActivity.java
@@ -2,14 +2,20 @@ package com.example.ilham.loginlogout.Activity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
+import com.daimajia.androidanimations.library.fading_entrances.FadeInAnimator;
+import com.daimajia.androidanimations.library.zooming_entrances.ZoomInAnimator;
 import com.example.ilham.loginlogout.R;
 import com.example.ilham.loginlogout.ViewPagerAdapter;
 
@@ -19,39 +25,45 @@ public class EntryActivity extends AppCompatActivity {
     private ViewPagerAdapter adapter;
     private Toolbar toolbar;
     private FloatingActionButton btnFloat1, btnFloat2;
-
+    private CardView cardView, cardView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entry);
-
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Entry");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         toolbar.setTitleTextColor(Color.parseColor("#ffffff"));
-
         tabLayout = (TabLayout) findViewById(R.id.entry_tablayout);
         viewPager = (ViewPager) findViewById(R.id.entry_viewpager);
         adapter = new ViewPagerAdapter(getSupportFragmentManager());
-
+        cardView = (CardView) findViewById(R.id.fab_sheet);
         btnFloat1 = (FloatingActionButton) findViewById(R.id.entry_fab1);
         btnFloat2 = (FloatingActionButton) findViewById(R.id.entry_fab2);
+
         btnFloat1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Snackbar.make(v, "Buka Snackbar 1", Snackbar.LENGTH_SHORT).show();
+                ButtonPressed();
+
             }
         });
 
         btnFloat2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Snackbar.make(v, "Buka Snackbar 2", Snackbar.LENGTH_SHORT).show();
+                ButtonPressed();
             }
         });
 
+        viewPager.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                closeCardView();
+            }
+        });
 
         tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
@@ -81,16 +93,35 @@ public class EntryActivity extends AppCompatActivity {
 
 
     }
-
+    public void closeCardView(){
+        YoYo.with(Techniques.SlideOutDown).duration(500).playOn(cardView);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                cardView.setVisibility(View.GONE);
+            }
+        }, 500);
+    }
+    public void ButtonPressed (){
+        if (cardView.getVisibility()==View.GONE){
+            cardView.setVisibility(View.VISIBLE);
+            YoYo.with(Techniques.SlideInUp).duration(500).playOn(cardView);
+        }
+        else{
+            closeCardView();
+        }
+    }
     private void animateFab(int position) {
         switch (position) {
             case 0:
                 btnFloat1.show();
                 btnFloat2.hide();
+                cardView.setVisibility(View.GONE);
                 break;
             case 1:
                 btnFloat2.show();
                 btnFloat1.hide();
+                cardView.setVisibility(View.GONE);
                 break;
 
             default:
@@ -101,8 +132,13 @@ public class EntryActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onSupportNavigateUp() {
-        onBackPressed();
-        return true;
+    public void onBackPressed() {
+        if(cardView.getVisibility()==View.VISIBLE){
+            closeCardView();
+        }
+        else{
+            super.onBackPressed();
+            return;
+        }
     }
 }
