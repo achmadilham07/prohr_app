33f5119bc931fc29a7146fc19b0d29a197374b45
alfinnur <36445393+alfinnur@users.noreply.github.com>
2018-03-19 18:22:20 +0700

    php
    add php file
    add uploader(otomatis upload php file ke hosting)

---
+++ php_file/Uploader.bat
@@ -0,0 +1,8 @@
+@echo off
+echo open ftp.urbancare.id> ftpcmd.bat
+echo urbancare@urbancare.id>> ftpcmd.bat
+echo J$ml~P4V,M~L>> ftpcmd.bat
+echo mput *.php>> ftpcmd.bat
+echo quit>> ftpcmd.bat
+ftp -i -s:ftpcmd.bat
+del ftpcmd.bat
